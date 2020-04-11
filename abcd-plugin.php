15abca4f9fca23496a6847b7e7203b7bcfd3669f
Nauman Bashir <nauman.bashir@hotmail.com>
2020-04-11 11:33:13 +0500

    Initial Commit

---
+++ abcd-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package           Abcd Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Abcd Plugin
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ **/
+
+ 
