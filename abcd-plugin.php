8b3f5a05b8acc502371088c282871ce2d1a45364
Nauman Bashir <nauman.bashir@hotmail.com>
2020-04-11 12:13:00 +0500

    Setting activation, deactivation

--- abcd-plugin.php
+++ abcd-plugin.php
@@ -10,4 +10,34 @@
  * Description:       Description of the plugin.
  **/
 
- 
+defined ('ABSPATH') or die('You don\'t have access to files');
+
+class abcdPlugin
+{
+    // Activation
+    protected function activate()
+    {
+
+    }
+
+    // Deactivation
+    protected function deactivate()
+    {
+        
+    }
+
+    // Uninstall
+    protected function uninstall()
+    {
+        
+    }
+
+}
+
+if ( class_exists('abcdPlugin') ) {
+    $abcdplugin = new abcdPlugin();
+}
+
+register_activation_hook( __FILE__ , array($abcdplugin, 'activate') );
+
+register_deactivation_hook( __FILE__ , array($abcdplugin, 'deactivate') );
